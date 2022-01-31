66cae58b15a85b83864e4887c8f35ae984379ac6
Felipe Prado <jfpc11@misena.edu.co>
2022-01-31 16:18:33 -0500

    adding using callbacks

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,11 +4,10 @@
 from typing import Callable
 
 
-def multiply(x) -> float:
-    """multiplies a number"""
-    return x * x
-
-
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """returns a function that multiplies a float by multiplier"""
+    def multiply(x: float) -> float:
+        """multiplies a number"""
+        return multiplier * x
+
     return multiply
