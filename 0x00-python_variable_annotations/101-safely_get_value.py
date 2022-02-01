5b825308900244329008e4953048f150f4e6d84f
Felipe Prado <jfpc11@misena.edu.co>
2022-01-31 21:57:52 -0500

    adding the union to the default

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -7,7 +7,8 @@ T = TypeVar("T")
 
 
 def safely_get_value(
-        dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+        dct: Mapping,
+        key: Any, default: Union[T, None] = None) -> Union[Any, T]:
     """returning a value"""
     if key in dct:
         return dct[key]
