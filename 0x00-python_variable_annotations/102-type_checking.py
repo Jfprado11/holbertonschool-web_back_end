3b0118cfff441fbcdde08cee1bb40d0459ac8434
Felipe Prado <jfpc11@misena.edu.co>
2022-01-31 22:05:37 -0500

    using mypy to check values

---
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""duck-typed annotations"""
+
+
+from typing import List, Tuple
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """returns a value of a list from a list"""
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(factor)
+    ]
+    return zoomed_in
+
+
+array = (12, 72, 91)
+
+zoom_2x = zoom_array(array)
+
+zoom_3x = zoom_array(array, 3)
