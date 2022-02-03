93a009b1ac8ca50673ff786ed0dd28f993bafaae
Felipe Prado <jfpc11@misena.edu.co>
2022-02-03 13:53:44 -0500

    store in caching using fifo algoti

---
+++ 0x03-caching/1-fifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Implementing the FIFO caching
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Class to represent a fifo cache"""
+
+    def __init__(self) -> None:
+        """Initialiaze the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """store data in the dict cache"""
+        if (key != None) and (item != None):
+            self.cache_data[key] = item
+            if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+                keys = next(iter(self.cache_data))
+                del self.cache_data[keys[0]]
+                print("DISCARD: {}".format(keys[0]))
+
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        """get the data looking for"""
+        if (key is None) or (key not in self.cache_data):
+            return None
+        return self.cache_data[key]
