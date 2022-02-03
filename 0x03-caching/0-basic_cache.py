9a94f95bd3faf137203e85576446b3160908e9a9
Felipe Prado <jfpc11@misena.edu.co>
2022-02-03 11:50:52 -0500

    adding the function to put and get into the cache

---
+++ 0x03-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Craetes a new class for a basic diccionary
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """a class to represent a cache"""
+
+    def __init__(self) -> None:
+        """iniatiliaze the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """declares data to the cache"""
+        if (key != None) and (item != None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """get the data looking for"""
+        if (key is None) or (key not in self.cache_data):
+            return None
+        return self.cache_data[key]
