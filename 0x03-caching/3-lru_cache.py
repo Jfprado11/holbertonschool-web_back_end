ae0aed15984a2a8c276ac486cafb394223893f31
Felipe Prado <jfpc11@misena.edu.co>
2022-02-05 16:00:14 -0500

    checking if it works

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Implementing the LIFO caching
+"""
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Class to represent a Lifo cache"""
+
+    def __init__(self) -> None:
+        """Initialiaze the class"""
+        super().__init__()
+
+    def put(self, key, item):
+        """store data in the dict cache"""
+        if (key is not None) and (item is not None):
+            self.cache_data[key] = item
+            if (len(self.cache_data) > BaseCaching.MAX_ITEMS):
+                last_item = list(self.cache_data)[-2]
+                del self.cache_data[last_item]
+                print("DISCARD: {}".format(last_item))
+
+    def get(self, key):
+        """get the data looking for"""
+        if (key is None) or (key not in self.cache_data):
+            return None
+        return self.cache_data[key]
