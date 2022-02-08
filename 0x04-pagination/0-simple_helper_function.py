58dc99bb59285bd524bbfdf44a90287463ce2a3b
Felipe Prado <jfpc11@misena.edu.co>
2022-02-08 10:06:03 -0500

    tring to figure out the requirement

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -6,8 +6,8 @@ Obtains the indexes for the pagination
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """return a tuple with indexes to a list of pagination"""
-    start_idx = page - 1
+    start_idx = page
     end_idx = start_idx + page_size
     return (start_idx, end_idx)
