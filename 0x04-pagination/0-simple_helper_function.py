da9d92c50fdc876a5d988851321e14ef99988dfd
Felipe Prado <jfpc11@misena.edu.co>
2022-02-08 10:03:04 -0500

    addding function to obtain idx

---
+++ 0x04-pagination/0-simple_helper_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Obtains the indexes for the pagination
+"""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """return a tuple with indexes to a list of pagination"""
+    start_idx = page - 1
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
