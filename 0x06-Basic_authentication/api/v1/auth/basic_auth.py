e6627451a45f627c1dcb6a82f6abc11b8543380b
Felipe Prado <jfpc11@misena.edu.co>
2022-02-15 12:09:21 -0500

    finding a new user

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,9 +3,10 @@
 """
 
 import binascii
-from typing import Tuple
+from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -48,3 +49,21 @@ class BasicAuth(Auth):
         username = decoded_base64_authorization_header[:position_index]
         password = decoded_base64_authorization_header[position_index + 1:]
         return username, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """user instace based on email and password
+        """
+        if (user_email is None) or (not isinstance(user_email, str)):
+            return None
+
+        if (user_pwd is None) or (not isinstance(user_pwd, str)):
+            return None
+
+        find_user = User.search({"email": user_email})
+        if (len(find_user) == 0):
+            return None
+
+        if (find_user[0].is_valid_password(user_pwd) == False):
+            return None
+        return find_user[0]
