57242747520c55df444bf52eb3909a1bee5ab760
Felipe Prado <jfpc11@misena.edu.co>
2022-02-15 10:25:13 -0500

    adding a method to obtain the string to be convert into base64

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,14 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """inherits from auth
     """
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extact a base 64
+        """
+        if (authorization_header is None) or (not isinstance(
+                authorization_header, str)):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
