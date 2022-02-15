b04b5a02adc3542b081d35bc2af5a7d628e15406
Felipe Prado <jfpc11@misena.edu.co>
2022-02-15 10:41:45 -0500

    adding a decode base 64

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,14 +3,15 @@
 """
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """inherits from auth
     """
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """extact a base 64
         """
         if (authorization_header is None) or (not isinstance(
@@ -19,3 +20,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode a base 64 auth header
+        """
+        if (base64_authorization_header is None) or (not isinstance(
+                base64_authorization_header, str)):
+            return None
+        try:
+            decodi = base64.b64decode(base64_authorization_header)
+            return decodi.decode("utf-8")
+        except:
+            return None
