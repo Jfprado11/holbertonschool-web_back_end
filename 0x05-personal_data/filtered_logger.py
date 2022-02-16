62ea941b2df566ac203cdec376fec76e1ad804c2
Felipe Prado <jfpc11@misena.edu.co>
2022-02-15 21:36:49 -0500

    using groups

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -14,6 +14,6 @@ def filter_datum(fields: List[str], redaction: str,
     """returns the log message obfuscated:"""
     new_log = message
     for field in fields:
-        new_log = re.sub(r'{}=.*?;'.format(field),
-                         r'{}={};'.format(field, redaction), new_log)
+        new_log = re.sub(r'({}=).*?;'.format(field),
+                         r'\1{};'.format(redaction), new_log)
     return new_log
