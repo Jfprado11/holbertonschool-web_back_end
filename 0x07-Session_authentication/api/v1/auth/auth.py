f80ab2307aef274de3398781b63d8881ce6072f4
Felipe Prado <jfpc11@misena.edu.co>
2022-02-16 12:55:22 -0500

    adding a route to get the user

---
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+"""
+handling the auth with a class
+"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """a class to check the basic auth to the paths
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """wether the path needs auth
+        """
+        if (path is None) or (excluded_paths is None or
+                              len(excluded_paths) == 0):
+            return True
+
+        if path[-1] == '/':
+            if path in excluded_paths:
+                return False
+            return True
+
+        path_slash = path + '/'
+        if path_slash in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """check if the header recive the auth
+        """
+        if (request is None):
+            return None
+
+        if "Authorization" not in request.headers.keys():
+            return None
+
+        return request.headers["Authorization"]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns the current log user
+        """
+        return None
