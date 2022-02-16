6ac22ba3b546230eceb3987e427b7b594efcde45
Felipe Prado <jfpc11@misena.edu.co>
2022-02-15 21:31:51 -0500

    checking tehe errors

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -9,7 +9,8 @@ from typing import List
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """returns the log message obfuscated:"""
     new_log = message
     for field in fields:
