b91c99adec6954bd3e615fb80fbacf1edcd8d1bd
Felipe Prado <jfpc11@misena.edu.co>
2022-02-16 21:52:49 -0500

    adding a new id using uuid

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """making a session auth class
 """
 
+import uuid
 from api.v1.auth.auth import Auth
 
 
@@ -9,5 +10,17 @@ class SessionAuth(Auth):
     """class to new auth session
     """
 
+    user_id_by_session_id = {}
+
     def __init__(self) -> None:
         super().__init__()
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates the session id
+        """
+        if (user_id is None) or (not isinstance(user_id, str)):
+            return None
+
+        new_id = str(uuid.uuid4())
+        self.user_id_by_session_id[new_id] = user_id
+        return new_id
