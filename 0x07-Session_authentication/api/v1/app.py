f0efaeb47d9a6b9e8a6f28af4a86e06d85598759
Felipe Prado <jfpc11@misena.edu.co>
2022-02-17 10:19:25 -0500

    adding routes to login

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -33,9 +33,12 @@ def before_request_func():
     """
     if auth is not None:
         endpoints = ['/api/v1/status/',
-                     '/api/v1/unauthorized/', '/api/v1/forbidden/']
+                     '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                     '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, endpoints) is True:
-            if auth.authorization_header(request) is None:
+
+            if ((auth.authorization_header(request) is None)
+                    and (auth.session_cookie(request) is None)):
                 abort(401)
             user = auth.current_user(request)
             if user is None:
