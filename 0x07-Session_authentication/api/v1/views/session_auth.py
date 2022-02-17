5d1744e18c7a7b7cbb8a905465a37b030df69d11
Felipe Prado <jfpc11@misena.edu.co>
2022-02-17 11:06:38 -0500

    setting a cookir based session auth{

---
+++ 0x07-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""
+Getting a new view to user session log in
+"""
+
+from crypt import methods
+from os import getenv
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login():
+    """get the session login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if (email is None) or (email == ""):
+        return jsonify({"error": "email missing"}), 400
+
+    if (password is None) or (password == ""):
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({"email": email})
+    if (len(user) == 0):
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if user[0].is_valid_password(password) is False:
+        return jsonify({"error": "wrong password"}), 401
+
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    out = jsonify(user[0].to_json())
+    out.set_cookie(getenv("SESSION_NAME"), session_id)
+    return out
