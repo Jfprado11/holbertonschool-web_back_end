0517db9dcf6e3ed88895e4d603ea2dfcd77b128e
Felipe Prado <jfpc11@misena.edu.co>
2022-02-17 22:11:37 -0500

    passing separator

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -13,6 +13,6 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """returns the log message obfuscated:"""
     for field in fields:
-        message = re.sub(r'({}=).*?;'.format(field),
+        message = re.sub(r'({}=).*?{}'.format(field, separator),
                          r'\1{}{}'.format(redaction, separator), message)
     return message
