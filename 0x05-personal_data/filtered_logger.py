c6d80a8c270a39a46fb2c8d3bc6d1b6a7990186e
Felipe Prado <jfpc11@misena.edu.co>
2022-02-17 23:27:08 -0500

    getitng the last function of the class

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -13,9 +13,9 @@ class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
     """
 
-    REDACTION: str = "***"
-    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR: str = ";"
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
 
     def __init__(self, fields: List[str]) -> None:
         """iniatilize the class
@@ -23,19 +23,20 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
-    def filter_datum(self, fields: List[str], redaction: str,
-                     message: str, separator: str) -> str:
-        """returns the log message obfuscated:"""
-        for field in fields:
-            message = re.sub(r'({}=).*?{}'.format(field, separator),
-                             r'\1{}{}'.format(redaction, separator), message)
-        return message
-
     def format(self, record: logging.LogRecord) -> str:
         """gets the format of a log
         """
-        message_obfs = self.filter_datum(
+        message_obfs = filter_datum(
             self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
         record.msg = message_obfs.replace(";", "; ")
         format_msg = logging.Formatter(self.FORMAT)
         return format_msg.format(record)
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """returns the log message obfuscated:"""
+    for field in fields:
+        message = re.sub(r'({}=).*?{}'.format(field, separator),
+                         r'\1{}{}'.format(redaction, separator), message)
+    return message
