d164d4ab8bb4ccc2a8313c0c7f7096b762a33e6f
Felipe Prado <jfpc11@misena.edu.co>
2022-02-22 20:15:04 -0500

    adding a method to get a hash password

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""hash a password"""
+
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """hash a password"""
+    hash_pasw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+    return hash_pasw
