b287fde8c5f35e8d0bbee1679594107bb26b1606
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 11:22:09 -0500

    adding the route for regiting a user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -5,7 +5,7 @@
 from flask import Flask, jsonify, request
 from auth import Auth
 
-
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +14,19 @@ def home():
     """home route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    "register two users"
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
