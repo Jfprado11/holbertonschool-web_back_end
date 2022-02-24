cb80c82480f941a8dc83f42989df22c2ece120f0
Felipe Prado <jfpc11@misena.edu.co>
2022-02-23 21:43:32 -0500

    using filter by

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -33,7 +33,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar("User"):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """adding a new user
         """
         new_user = User(email=email, hashed_password=hashed_password)
