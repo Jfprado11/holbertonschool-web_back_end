0b584c6e973206e3c2e5bf266cae5913e71095c4
Felipe Prado <jfpc11@misena.edu.co>
2022-02-23 22:19:37 -0500

    using other terms

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -17,18 +17,5 @@ def home():
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'])
-def users():
-    """register two users"""
-    email = request.form.get('email')
-    password = request.form.get('password')
-
-    try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-
-
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
