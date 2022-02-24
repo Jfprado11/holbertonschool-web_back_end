15f07b28ff7a3c619d104b01f89d768f8928e411
Felipe Prado <jfpc11@misena.edu.co>
2022-02-23 22:24:27 -0500

    fixing annotation

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -36,9 +36,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """adding a new user
         """
+        session = self._session()
         new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
+        session.add(new_user)
+        session.commit()
         return new_user
 
     def find_user_by(self, **args) -> User:
