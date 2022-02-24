7f355c7ee69bf43964988f8360b8209489181ad5
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 11:31:08 -0500

    adding to check a password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -25,6 +25,15 @@ class Auth:
             pwd = _hash_password(password)
             self._db.add_user(email, pwd)
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """validate the password
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """hash a password"""
