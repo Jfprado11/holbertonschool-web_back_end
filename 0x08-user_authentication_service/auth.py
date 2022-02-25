5ae3ce94abde51066b7dc3187cc5ea2cc933d7c7
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 22:01:21 -0500

    verifing an error

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-"""hash a password"""
-
-from typing import Union
-import uuid
+"""Auth module
+"""
 import bcrypt
 from db import DB
-from sqlalchemy.orm.exc import NoResultFound
-
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
 class Auth:
@@ -18,27 +16,29 @@ class Auth:
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """returns the register user
-        """
-        try:
-            self._db.find_user_by(email=email)
-            raise ValueError("User {} already exists".format(email))
-        except NoResultFound:
-            pwd = _hash_password(password)
-            self._db.add_user(email, pwd)
-
-    def valid_login(self, email: str, password: str) -> bool:
-        """validate the password
+        """Method that checks if a user already exists
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
+            raise ValueError('User {} already exists'.format(email))
+        except NoResultFound:
+            passw = _hash_password(password)
+            return self._db.add_user(email, passw)
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Method that locates a user by email and checks
+        the password
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode(
+                    'utf-8'), user.hashed_password):
+                return True
         except NoResultFound:
             return False
 
     def create_session(self, email: str) -> str:
-        """creates a session id
+        """Method that generate a session ID and store it in the database
         """
         try:
             user = self._db.find_user_by(email=email)
