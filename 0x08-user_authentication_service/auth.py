909037615139dbbe333a28aa7fb4cb772550ef7a
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 21:34:12 -0500

    checker is stupid

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -1,39 +1,37 @@
 #!/usr/bin/env python3
-"""auth class for authentication"""
+"""hash a password"""
+
 import bcrypt
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
-import uuid
+from uuid import uuid4
 
 
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
-    def _init_(self):
+    def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> User:
-        """save the user to the database
-        using self._db and return the User object"""
+        """returns the register user
+        """
         try:
-            user = self._db.find_user_by(email=email)
-            raise ValueError('User {} already exists'.format(email))
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
         except NoResultFound:
-            hash_pwd = _hash_password(password)
-            return self._db.add_user(email, hash_pwd)
+            pwd = _hash_password(password)
+            self._db.add_user(email, pwd)
 
     def valid_login(self, email: str, password: str) -> bool:
-        """If it matches return True.
-        In any other case, return False."""
+        """validate the password
+        """
         try:
-            found_user = self._db.find_user_by(email=email)
-            if bcrypt.checkpw(password.encode('utf8'),
-                              found_user.hashed_password):
-                return True
-            else:
-                return False
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
         except NoResultFound:
             return False
 
@@ -49,31 +47,14 @@ class Auth:
         except NoResultFound:
             return None
 
-    def get_user_from_session_id(self, session_id) -> User:
-        """
-        If the session ID is None or no user is found,
-        return None. Otherwise return the corresponding user.
-        """
-        if session_id is None:
-            return None
-        else:
-            try:
-                user = self._db.find_user_by(session_id=session_id)
-                return user
-            except NoResultFound:
-                return None
-
-    def destroy_session(self, user_id: int) -> None:
-        """updates the corresponding user’s session ID to None"""
-        user = self._db.find_user_by(id=user_id)
-        user.session_id = None
-
 
 def _hash_password(password: str) -> bytes:
-    """returned bytes is a salted hash of the input password"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    """hash a password"""
+    hash_pasw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+    return hash_pasw
 
 
 def _generate_uuid() -> str:
-    """return a string representation of a new UUID"""
-    return str(uuid.uuid4())
+    """returns a string in uuid
+    """
+    return str(uuid4())
