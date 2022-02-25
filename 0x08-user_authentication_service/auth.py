14b12f23049b4bedbf1e8962d0eff372c097bc51
Felipe Prado <jfpc11@misena.edu.co>
2022-02-25 11:46:46 -0500

    removes the session id of a user

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -58,6 +58,12 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """destroy the session of a user
+        """
+        self._db.update_user(user_id=user_id, session_id=None)
+        return None
+
 
 def _hash_password(password: str) -> bytes:
     """hash a password"""
