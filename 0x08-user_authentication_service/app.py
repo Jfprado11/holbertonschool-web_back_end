7c7dc0c89037f37378850090ab5779d6326190bb
Felipe Prado <jfpc11@misena.edu.co>
2022-02-25 12:01:04 -0500

    gerrign the profile'

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -60,5 +60,16 @@ def delete_session():
     return redirect('/')
 
 
+@app.route('/profile', methods=["GET"])
+def profile():
+    """gets the profile route to the logged user
+    """
+    session = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id=session)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
