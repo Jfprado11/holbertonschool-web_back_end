1a1f44d0e62cefc182564e86d310af929be25a98
Felipe Prado <jfpc11@misena.edu.co>
2022-02-25 14:13:03 -0500

    reseting a new password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -75,6 +75,17 @@ class Auth:
         except NoResultFound:
             raise ValueError
 
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ upodates the password
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            new_pwd = _hash_password(password)
+            self._db.update_user(
+                user.id, hashed_password=new_pwd, reset_token=None)
+        except NoResultFound:
+            raise ValueError
+
 
 def _hash_password(password: str) -> bytes:
     """hash a password"""
