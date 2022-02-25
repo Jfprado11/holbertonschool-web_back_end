9437998ae4e0dec019b1b14ffda0d07e71baac06
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 21:37:27 -0500

    checker is stupid

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -38,14 +38,12 @@ class Auth:
             return False
 
     def create_session(self, email: str) -> str:
-        """ It takes an email string argument
-        and returns the session ID as a string"""
+        """Method that generate a session ID and store it in the database
+        """
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            user.session_id = session_id
-            self._db._session.commit()
-            return session_id
+            self._db.update_user(user.id, session_id=_generate_uuid())
+            return user.session_id
         except NoResultFound:
             return None
 
