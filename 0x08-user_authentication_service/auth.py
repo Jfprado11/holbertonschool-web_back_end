fec94a10a31542fe98cc87b8376896375bc9520d
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 22:39:12 -0500

    finding user by its id

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -47,6 +47,17 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
+        """ returns the corresponding User or None by its id
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """hash a password"""
