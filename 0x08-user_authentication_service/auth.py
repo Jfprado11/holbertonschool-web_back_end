969baca85431d1509e464093238d433dd770f15b
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 21:19:11 -0500

    withour new function

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -37,15 +37,15 @@ class Auth:
         except NoResultFound:
             return False
 
-    def create_session(self, email: str) -> str:
-        """creates a session id
-        """
-        try:
-            user = self._db.find_user_by(email=email)
-            self._db.update_user(user.id, session_id=_generate_uuid())
-            return user.session_id
-        except NoResultFound:
-            return
+    # def create_session(self, email: str) -> str:
+    #     """creates a session id
+    #     """
+    #     try:
+    #         user = self._db.find_user_by(email=email)
+    #         self._db.update_user(user.id, session_id=_generate_uuid())
+    #         return user.session_id
+    #     except NoResultFound:
+    #         return
 
 
 def _hash_password(password: str) -> bytes:
