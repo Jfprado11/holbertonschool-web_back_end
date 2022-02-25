ddc2dc4384a22f88b2b280c83ea00144eef288fa
Felipe Prado <jfpc11@misena.edu.co>
2022-02-24 21:57:41 -0500

    adding to create a session

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,6 +2,7 @@
 """Creating a user auth service
 """
 
+from os import abort
 from flask import Flask, jsonify, request
 from auth import Auth
 
@@ -27,6 +28,22 @@ def users():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=["POST"])
+def sessios():
+    """route for creating the sessions
+    """
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if(AUTH.valid_login(email=email, password=password)):
+        new_session = AUTH.create_session(email=email)
+        resp = jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie("session_id", new_session)
+        return resp
+    abort(401)
 
 
 if __name__ == "__main__":
