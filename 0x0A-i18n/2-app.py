6b8404738fd99091b3122fcf4674d5a2beb647e4
Felipe Prado <jfpc11@misena.edu.co>
2022-03-03 11:34:39 -0500

    chcking the best match for language

---
+++ 0x0A-i18n/2-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""Flask app to trasnlate i18n
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config ():
+    """config the lang for the app
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """locate the language and timezone for the app
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def home():
+    """home page of flask app
+    """
+    return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
