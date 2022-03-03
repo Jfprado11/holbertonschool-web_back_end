33ee833f1c41ea5f246c58d0e0adfc60f94e85fd
Felipe Prado <jfpc11@misena.edu.co>
2022-03-03 10:52:10 -0500

    Adding to run the server

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -6,8 +6,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     """home page of flask app
     """
     return render_template("0-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
