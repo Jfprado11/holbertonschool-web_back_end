76f4203294a8156a7b91c2b7c96552a48a7a40fb
Felipe Prado <jfpc11@misena.edu.co>
2022-03-03 19:52:46 -0500

    passing a py

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -3,7 +3,7 @@
 """
 
 from flask import Flask, render_template, request
-from flask_babel import Babel, _
+from flask_babel import Babel
 
 
 class Config ():
@@ -15,15 +15,12 @@ class Config ():
 
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+
 babel = Babel(app)
 
 
-@app.route('/')
-def home():
-    """home page of flask app
-    """
-    return render_template("3-index.html")
+app.config.from_object(Config)
 
 
 @babel.localeselector
@@ -33,5 +30,12 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-if __name__ == "_main_":
-    app.run(debug=True)
+@app.route('/')
+def home():
+    """home page of flask app
+    """
+    return render_template("3-index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
