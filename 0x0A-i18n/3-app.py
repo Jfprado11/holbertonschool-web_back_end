e1a3e33d075f4e60f769cc65d13d6ab348b08c95
Felipe Prado <jfpc11@misena.edu.co>
2022-03-03 19:51:53 -0500

    passing a py

--- 0x0A-i18n/3-app.py
+++ 0x0A-i18n/3-app.py
@@ -33,5 +33,5 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == "_main_":
+    app.run(debug=True)
