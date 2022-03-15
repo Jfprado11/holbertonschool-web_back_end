ea038cd5409390401bbdfe4b59767d38e9f5bf14
Felipe Prado <jfpc11@misena.edu.co>
2022-03-15 10:36:23 -0500

    loking for the erros

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -20,6 +20,6 @@ class Cache():
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores the data into the redis application
         """
-        # key = str(uuid.uuid4())
+        key = str(uuid.uuid4())
         self._redis.set("key", data)
-        return "key"
+        return key
