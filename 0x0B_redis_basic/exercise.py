82a443a96b6a78c0363b2ca8f59b1fd3b01159a2
Felipe Prado <jfpc11@misena.edu.co>
2022-03-15 10:23:34 -0500

    loking for missing check:

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -12,12 +12,12 @@ class Cache():
     """
 
     def __init__(self) -> None:
-        self._redis = redis.Redis()
-        self._redis.flushdb
+        _redis = redis.Redis()
+        _redis.flushdb
 
-    def store(self, data) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores the data into the redis application
         """
         key = str(uuid.uuid4())
-        self._redis.set(key, data)
+        redis.Redis().set(key, data)
         return key
