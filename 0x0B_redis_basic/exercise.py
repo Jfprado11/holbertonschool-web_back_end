67d82b12bf9f99c287bc1f5fa5a067f900e27d19
Felipe Prado <jfpc11@misena.edu.co>
2022-03-14 22:29:10 -0500

    adding the set to a value

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Creating a class for using with redis
+"""
+
+from typing import Union
+import uuid
+import redis
+
+
+class Cache():
+    """a class for the cache using the redis tool
+    """
+
+    def __init__(self) -> None:
+        self._redis = redis.Redis()
+        self._redis.flushdb
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """stores the data into the redis application
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
