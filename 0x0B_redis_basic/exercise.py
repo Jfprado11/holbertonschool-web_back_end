90bda47f08f4b701fe2e1931876648df9e1529bd
Felipe Prado <jfpc11@misena.edu.co>
2022-03-15 22:27:21 -0500

    passing the arguments compiled

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -8,65 +8,65 @@ import uuid
 import redis
 
 
-def count_calls(method: Callable) -> Callable:
-    """decorator to see how many times it was called
-    """
-    key_name = method.__qualname__
+# def count_calls(method: Callable) -> Callable:
+#     """decorator to see how many times it was called
+#     """
+#     key_name = method.__qualname__
 
-    @wraps(method)
-    def calls_made(self, data):
-        """count the times the method was called
-        """
-        stored = self._redis.get(key_name)
-        if stored is None:
-            self._redis.set(key_name, 1)
-        else:
-            self._redis.incr(key_name)
-        return method(self, data)
-    return calls_made
-
-
-def call_history(method: Callable) -> Callable:
-    """a decorator to the input and outpus
-    """
-    key_name_inputs = method.__qualname__ + ":inputs"
-    key_name_outpus = method.__qualname__ + ":outputs"
-
-    @wraps(method)
-    def create_lists(self, *data):
-        """creating the lists for outputs and inputs
-        """
-        data = str(data)
-        self._redis.rpush(key_name_inputs, data)
-        output_data = method(self, data)
-        self._redis.rpush(key_name_outpus, output_data)
-        return output_data
-
-    return create_lists
+#     @wraps(method)
+#     def calls_made(self, data):
+#         """count the times the method was called
+#         """
+#         stored = self._redis.get(key_name)
+#         if stored is None:
+#             self._redis.set(key_name, 1)
+#         else:
+#             self._redis.incr(key_name)
+#         return method(self, data)
+#     return calls_made
 
 
-def replay(fn: Callable) -> None:
-    """display the callables of the funcion
-    """
-    r = redis.Redis()
-    name_method = fn.__qualname__
-    times = r.get(name_method)
-    times = int(times)
-    print("{} was called {} times:".format(name_method, times))
+# def call_history(method: Callable) -> Callable:
+#     """a decorator to the input and outpus
+#     """
+#     key_name_inputs = method.__qualname__ + ":inputs"
+#     key_name_outpus = method.__qualname__ + ":outputs"
 
-    key_inp = name_method + ":inputs"
-    key_out = name_method + ":outputs"
-    inputs = r.lrange(key_inp, 0, -1)
-    inputs = [x.decode("utf-8") for x in inputs]
+#     @wraps(method)
+#     def create_lists(self, *data):
+#         """creating the lists for outputs and inputs
+#         """
+#         data = str(data)
+#         self._redis.rpush(key_name_inputs, data)
+#         output_data = method(self, data)
+#         self._redis.rpush(key_name_outpus, output_data)
+#         return output_data
 
-    outputs = r.lrange(key_out, 0, -1)
-    outputs = [x.decode("utf-8") for x in outputs]
+#     return create_lists
 
-    combined = zip(inputs, outputs)
-    combined = list(combined)
 
-    for item in combined:
-        print("{}(*{}) -> {}".format(name_method, item[0], item[1]))
+# def replay(fn: Callable) -> None:
+#     """display the callables of the funcion
+#     """
+#     r = redis.Redis()
+#     name_method = fn.__qualname__
+#     times = r.get(name_method)
+#     times = int(times)
+#     print("{} was called {} times:".format(name_method, times))
+
+#     key_inp = name_method + ":inputs"
+#     key_out = name_method + ":outputs"
+#     inputs = r.lrange(key_inp, 0, -1)
+#     inputs = [x.decode("utf-8") for x in inputs]
+
+#     outputs = r.lrange(key_out, 0, -1)
+#     outputs = [x.decode("utf-8") for x in outputs]
+
+#     combined = zip(inputs, outputs)
+#     combined = list(combined)
+
+#     for item in combined:
+#         print("{}(*{}) -> {}".format(name_method, item[0], item[1]))
 
 
 class Cache():
@@ -79,8 +79,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
-    @count_calls
+    # @call_history
+    # @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """stores the data into the redis application
         """
