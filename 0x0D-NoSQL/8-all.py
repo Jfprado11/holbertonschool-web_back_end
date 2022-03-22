229b130ba9c395054182401549e07b8816b2c77c
Felipe Prado <jfpc11@misena.edu.co>
2022-03-22 15:25:13 -0500

    retriving all items in mongfo

--- 0x0D-NoSQL/8-all.py
+++ 0x0D-NoSQL/8-all.py
@@ -7,6 +7,6 @@ def list_all(mongo_collection):
     """return all the collections obtained
     """
     documents = mongo_collection.find()
-    if documents.retrieved == 0:
+    if mongo_collection.count_documents == 0:
         return []
     return documents
