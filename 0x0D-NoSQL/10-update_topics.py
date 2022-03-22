c1100a0a9aee02925d80f85b102bb7daef71b475
Felipe Prado <jfpc11@misena.edu.co>
2022-03-22 18:55:43 -0500

    adding to change data

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -6,6 +6,6 @@
 def update_topics(mongo_collection, name, topics):
     """ updates a specifi item in a mongo docuement
     """
-    data = mongo_collection.update_one(
-        {"name": name}, {"$set": {"topics": topics}}, upsert=True)
+    data = mongo_collection.update_many(
+        {"name": name}, {"$set": {"topics": topics}})
     print(data.acknowledged)
