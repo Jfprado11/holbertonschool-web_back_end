76d128fa5c6ccde6a8beb0069a6603083b8bff96
Felipe Prado <jfpc11@misena.edu.co>
2022-03-24 13:52:27 -0500

    using for lloop

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,6 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const idx of array) {
     const value = idx;
+    // eslint-disable-next-line no-param-reassign
     array[array.indexOf(value)] = appendString + value;
   }
 
