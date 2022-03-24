899059bacf55536a65ab9dddef168fd37ed928f5
Felipe Prado <jfpc11@misena.edu.co>
2022-03-24 10:04:05 -0500

    passing the for of

---
+++ 0x0E-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-param-reassign */
+export default function appendToEachArrayValue(array, appendString) {
+  for (const [idx, value] of array.entries()) {
+    array[idx] = appendString + value;
+  }
+
+  return array;
+}
