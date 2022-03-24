dc717e8e49af95909b7352f3eda0cb1513ac3951
Felipe Prado <jfpc11@misena.edu.co>
2022-03-24 13:49:23 -0500

    using for lloop

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,7 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  let i = 0;
   for (const idx of array) {
-    const value = idx;
-    array.push(appendString + value);
+    const value = array[idx];
+    array[i] = appendString + value;
+    i += 1;
   }
 
   return array;
