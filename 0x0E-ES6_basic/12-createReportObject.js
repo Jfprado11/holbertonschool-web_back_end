5456c11816ce1bd5cee6adb5b0732cf92f0c97a6
Felipe Prado <jfpc11@misena.edu.co>
2022-03-24 17:23:19 -0500

    using to get the keys

---
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const data = {
+    allEmployees: employeesList,
+    getNumberOfDepartments(list) {
+      return Object.keys(list).length;
+    },
+  };
+  return data;
+}
