c40f39f4dc3f10fd4355299a209da26529827adf
Felipe Prado <jfpc11@misena.edu.co>
2022-03-29 11:50:42 -0500

    passing values

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -5,6 +5,14 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const allArray = Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((values) => {
+    values.forEach((elements) => {
+      if (Object.keys(elements).includes('reason')) {
+        Object.assign(elements, { value: elements.reason });
+        delete elements.reason;
+      }
+    });
+    return values;
+  });
   return allArray;
 }
