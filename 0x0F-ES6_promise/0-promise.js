7d8937422b766d0ad7babb950c2c4a7e8ec8e198
Felipe Prado <jfpc11@misena.edu.co>
2022-03-28 23:09:18 -0500

    passing a promise

---
+++ 0x0F-ES6_promise/0-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('hey');
+    } else {
+      reject(new Error('error'));
+    }
+  });
+}
