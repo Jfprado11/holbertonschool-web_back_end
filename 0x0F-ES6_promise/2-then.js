bf0d54c03b33b87ae631b3d8ef087469ef94bacd
Felipe Prado <jfpc11@misena.edu.co>
2022-03-29 10:08:46 -0500

    passing a promise

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -2,12 +2,13 @@ export default function handleResponseFromAPI(promise) {
   let isResolved = null;
   promise
     .then(() => {
-      console.log('Got a response from the API');
       isResolved = { status: 200, body: 'success' };
     })
     .catch(() => {
+      isResolved = new Error('');
+    })
+    .finally(() => {
       console.log('Got a response from the API');
-      isResolved = new Error();
     });
   return isResolved;
 }
