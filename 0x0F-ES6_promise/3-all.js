f40e572f8c6245704c6cfe85e19113b48d1c683d
Felipe Prado <jfpc11@misena.edu.co>
2022-03-29 13:41:15 -0500

    passing all the values

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  let val = null;
   try {
-    val = await Promise.all([uploadPhoto(), createUser()]);
-    console.log(val[0].body, val[1].firstName, val[1].lastName);
+    const userupload = await createUser();
+    const photo = await uploadPhoto();
+    console.log(`${photo.body} ${userupload.firstName} ${userupload.lastName}`);
   } catch (err) {
     console.log('Signup system offline');
   }
-  return val;
 }
