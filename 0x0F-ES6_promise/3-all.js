0dd08a3ebc40eae075351d498708004d0d8e01fc
Felipe Prado <jfpc11@misena.edu.co>
2022-03-29 10:38:51 -0500

    using promise all holberotn hell

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,6 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function handleProfileSignup() {
-  const val = await Promise.all([uploadPhoto(), createUser()]);
-  console.log(val[0].body, val[1].firstName, val[1].lastName);
+  try {
+    const val = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(val[0].body, val[1].firstName, val[1].lastName);
+  } catch (err) {
+    console.log('Signup system offline');
+  }
 }
