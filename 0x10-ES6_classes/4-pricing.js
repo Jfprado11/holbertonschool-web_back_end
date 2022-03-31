2dc3e9539a8006241dac7d9ef0d271a9060c1b08
Felipe Prado <jfpc11@misena.edu.co>
2022-03-31 14:09:33 -0500

    passing a static method

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newValue) {
+    this._amount = newValue;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newValue) {
+    this._currency = newValue;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
