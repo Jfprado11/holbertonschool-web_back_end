5b0d2b33f8d9ba2e58bf03dab07ea910daaf3542
Felipe Prado <jfpc11@misena.edu.co>
2022-04-05 17:51:29 -0500

    converting from an set into a tring

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,6 @@
 export default function cleanSet(set, startString) {
   const array = [];
-  if (startString.length === 0) return '';
+  if (startString.length === 0 || typeof startString !== 'string') return '';
   set.forEach((element) => {
     if (element.startsWith(startString)) {
       array.push(element.slice(startString.length));
