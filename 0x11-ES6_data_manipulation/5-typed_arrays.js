a34cc842e615eab32f7d0da5891ce3835f3a0e6a
Felipe Prado <jfpc11@misena.edu.co>
2022-04-05 14:11:40 -0500

    craeting a dataview buffer

--- 0x11-ES6_data_manipulation/5-typed_arrays.js
+++ 0x11-ES6_data_manipulation/5-typed_arrays.js
@@ -1,9 +1,10 @@
 export default function createInt8TypedArray(length, position, value) {
-  const buffer = new ArrayBuffer(length);
   if (value > 127 || value < -128) {
     throw new Error('Position outside range');
   }
-  const arrayTyped = new Int8Array(buffer);
-  arrayTyped[position] = value;
-  return arrayTyped;
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  view.setInt8(position, value);
+
+  return view;
 }
