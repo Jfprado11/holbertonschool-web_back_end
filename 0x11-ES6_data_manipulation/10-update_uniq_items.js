fbd099e96601c40482eaa4c5944ec0137ddbb309
Felipe Prado <jfpc11@misena.edu.co>
2022-04-05 18:34:31 -0500

    using a map to update but checking for a check

--- 0x11-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x11-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,7 @@
 export default function updateUniqueItems(mapCheck) {
+  if (mapCheck instanceof Map === false) {
+    throw new Error('Cannot process');
+  }
   mapCheck.forEach((value, key) => {
     if (value === 1) {
       mapCheck.set(key, 100);
