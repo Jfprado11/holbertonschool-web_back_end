6f714758042c23ef7e1a20e1a368d4d26e81b7e5
Felipe Prado <jfpc11@misena.edu.co>
2022-04-06 10:44:39 -0500

    checking the clean

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,4 @@
-export default function cleanSet(set, startString) {
+export default function cleanSet(set, startString = '') {
   const array = [];
   if (startString.length === 0 || typeof startString !== 'string') return '';
   set.forEach((element) => {
