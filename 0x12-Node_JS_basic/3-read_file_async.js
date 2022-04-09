53bad059510542a6cea00811b0ab46fb90a69ca5
Felipe Prado <jfpc11@misena.edu.co>
2022-04-09 10:40:11 -0500

    task 3 checking

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -10,7 +10,20 @@ function countStudents(path) {
       const dataSeparated = dataGot.filter((items) => items !== '').map((item) => item.split(','));
       dataSeparated.shift();
       console.log(`Number of students: ${dataSeparated.length}`);
-      return resolve(dataSeparated);
+      const acum = {};
+      for (const student of dataSeparated) {
+        const keys = Object.keys(acum);
+        if (keys.includes(student[3])) {
+          acum[student[3]].push(student[0]);
+        } else {
+          acum[student[3]] = [student[0]];
+        }
+      }
+      const keysFinal = Object.keys(acum);
+      keysFinal.forEach((item) => {
+        console.log(`Number of students in ${item}: ${acum[item].length}. List: ${acum[item].join(', ')}`);
+      });
+      return resolve(acum);
     });
   });
 }
