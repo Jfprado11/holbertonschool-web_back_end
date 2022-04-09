d6947097fd6afe45766cc29d94574bc97db9ad1b
Felipe Prado <jfpc11@misena.edu.co>
2022-04-09 10:19:14 -0500

    loking for an error with reduce

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -4,16 +4,14 @@ function countStudents(path) {
   try {
     const fileCsv = fs.readFileSync(path, { encoding: 'utf8' });
     const dataGot = fileCsv.split('\n');
-    const dataSeparated = dataGot.filter((items) => items !== '').map((item) => item.split(','));
-    dataSeparated.shift();
-    const finalData = dataSeparated.reduce((acum, student) => {
-      if (Object.keys(acum).includes(student[3])) {
-        acum[student[3]].push(student[0]);
-      } else {
-        return { ...acum, [student[3]]: [student[0]] };
-      }
-      return acum;
-    }, {});
+    const dataSeparated = dataGot
+      .filter((items) => items !== '')
+      .map((item) => item.split(','))
+      .shift();
+    console.log(`Number of students: ${dataSeparated.length}`);
+    for (const student in dataSeparated) {
+      console.log(student);
+    }
   } catch (err) {
     throw new Error('Cannot load the database');
   }
