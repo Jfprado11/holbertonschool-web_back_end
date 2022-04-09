7f81164b5c539776e33e39f2d00b33205cbfa758
Felipe Prado <jfpc11@misena.edu.co>
2022-04-09 10:27:35 -0500

    loking for an error

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -4,18 +4,13 @@ function countStudents(path) {
   try {
     const fileCsv = fs.readFileSync(path, { encoding: 'utf8' });
     const dataGot = fileCsv.split('\n');
-    const dataSeparated = dataGot
-      .filter((items) => items !== '')
-      .map((item) => item.split(','))
-      .shift();
+    const dataSeparated = dataGot.filter((items) => items !== '').map((item) => item.split(','));
+    dataSeparated.shift();
     console.log(`Number of students: ${dataSeparated.length}`);
     const acum = {};
-    for (const student in dataSeparated) {
-      if (Object.keys(dataSeparated).includes(student[3])) {
-        acum[student[3]].push(student[0]);
-      } else {
-        return { ...acum, [student[3]]: [student[0]] };
-      }
+    const keys = Object.keys(dataSeparated);
+    for (const student of dataSeparated) {
+      console.log(student);
     }
   } catch (err) {
     throw new Error('Cannot load the database');
