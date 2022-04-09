eace7810ccefe87bb8a67f7c33e453aa0489a9bb
Felipe Prado <jfpc11@misena.edu.co>
2022-04-09 10:38:14 -0500

    task 3 checking

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -7,25 +7,21 @@ function countStudents(path) {
         return reject(new Error('Cannot load the database'));
       }
       const dataGot = data.split('\n');
-      const dataSeparated = dataGot
-        .filter((items) => items !== '')
-        .map((item) => item.split(','));
+      const dataSeparated = dataGot.filter((items) => items !== '').map((item) => item.split(','));
       dataSeparated.shift();
       console.log(`Number of students: ${dataSeparated.length}`);
       const finalData = dataSeparated.reduce((acum, student) => {
-        if (Object.keys(acum).includes(student[3])) {
+        const keys = Object.keys(acum);
+        if (keys.includes(student[3])) {
           acum[student[3]].push(student[0]);
         } else {
           return { ...acum, [student[3]]: [student[0]] };
         }
         return acum;
       }, {});
-      Object.keys(finalData).forEach((item) => {
-        console.log(
-          `Number of students in ${item}: ${
-            finalData[item].length
-          }. List: ${finalData[item].join(', ')}`,
-        );
+      const finalKeys = Object.keys(finalData);
+      finalKeys.forEach((item) => {
+        console.log(`Number of students in ${item}: ${finalData[item].length}. List: ${finalData[item].join(', ')}`);
       });
       return resolve(finalData);
     });
