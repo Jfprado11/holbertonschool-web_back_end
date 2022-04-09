26cacb73a31b2c8b6e4a2fc722ff86da3c372b22
Felipe Prado <jfpc11@misena.edu.co>
2022-04-08 20:49:37 -0500

    reading and getting the data from csv

---
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const fileCsv = fs.readFileSync(path, { encoding: 'utf8' });
+    const data = fileCsv.split('\n');
+    const dataSeparated = data
+      .filter((items) => items !== '')
+      .map((item) => item.split(','));
+    dataSeparated.shift();
+    console.log(`Number of students: ${dataSeparated.length}`);
+    const finalData = dataSeparated.reduce((acum, student) => {
+      if (Object.keys(acum).includes(student[3])) {
+        acum[student[3]].push(student[0]);
+      } else {
+        return { ...acum, [student[3]]: [student[0]] };
+      }
+      return acum;
+    }, {});
+    Object.keys(finalData).forEach((item) => {
+      console.log(
+        `Number of students in ${item}: ${
+          finalData[item].length
+        }. List: ${finalData[item].join(', ')}`,
+      );
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
