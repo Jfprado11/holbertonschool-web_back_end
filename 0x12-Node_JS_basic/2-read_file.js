805a9f1cacafec2e58df926eb067555a2b2f279c
Felipe Prado <jfpc11@misena.edu.co>
2022-04-09 10:13:33 -0500

    loking for an error

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 function countStudents(path) {
   try {
-    const fileCsv = fs.readFileSync(path);
+    const fileCsv = fs.readFileSync(path, { encoding: 'utf8' });
   } catch (err) {
     throw new Error('Cannot load the database');
   }
