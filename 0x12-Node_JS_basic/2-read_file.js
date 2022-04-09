a54b0543cf0b27f61bbc852c143b2888b2dcaaf0
Felipe Prado <jfpc11@misena.edu.co>
2022-04-09 10:16:39 -0500

    loking for an error with reduce

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -7,6 +7,14 @@ function countStudents(path) {
     const dataSeparated = dataGot.filter((items) => items !== '').map((item) => item.split(','));
     dataSeparated.shift();
     console.log(`Number of students: ${dataSeparated.length}`);
+    const finalData = dataSeparated.reduce((acum, student) => {
+      if (Object.keys(acum).includes(student[3])) {
+        acum[student[3]].push(student[0]);
+      } else {
+        return { ...acum, [student[3]]: [student[0]] };
+      }
+      return acum;
+    }, {});
   } catch (err) {
     throw new Error('Cannot load the database');
   }
