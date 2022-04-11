7e25c3c3c38fbaee8f9cd4357e44a6a3c94ab6fe
Felipe Prado <jfpc11@misena.edu.co>
2022-04-11 10:51:23 -0500

    using write and end

--- 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x12-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -4,19 +4,21 @@ const db = process.argv[2];
 
 export default class StudentsController {
   static async getAllStudents(request, response) {
+    const arrayToSend = [];
     try {
-      response.write('This is the list of our students\n');
+      arrayToSend.push('This is the list of our students\n');
       const data = await readDatabase(db);
+      delete data.numOfStudents;
       const keysFinal = Object.keys(data);
       keysFinal.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
       keysFinal.forEach((item, idx) => {
         if (idx === keysFinal.length - 1) {
-          response.write(`Number of students in ${item}: ${data[item].length}. List: ${data[item].join(', ')}`);
+          arrayToSend.push(`Number of students in ${item}: ${data[item].length}. List: ${data[item].join(', ')}`);
         } else {
-          response.write(`Number of students in ${item}: ${data[item].length}. List: ${data[item].join(', ')}\n`);
+          arrayToSend.push(`Number of students in ${item}: ${data[item].length}. List: ${data[item].join(', ')}\n`);
         }
       });
-      response.status(200).end();
+      response.status(200).send(arrayToSend.join(''));
     } catch (err) {
       response.status(500).send('Cannot load the database');
     }
@@ -25,13 +27,14 @@ export default class StudentsController {
   static async getAllStudentsByMajor(request, response) {
     const param = request.params.major;
     if (param === 'CS' || param === 'SWE') {
-      try {
-        const data = await readDatabase(db);
-        const students = data[param];
-        response.status(200).send(`List: ${students.join(', ')}`);
-      } catch (err) {
-        response.status(500).send('Cannot load the database');
-      }
+      readDatabase(db)
+        .then((data) => {
+          const students = data[param];
+          response.status(200).send(`List: ${students.join(', ')}`);
+        })
+        .catch(() => {
+          response.status(500).send('Cannot load the database');
+        });
     } else {
       response.status(500).send('Major parameter must be CS or SWE');
     }
